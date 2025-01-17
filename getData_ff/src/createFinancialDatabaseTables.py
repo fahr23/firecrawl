804e06d463a604a55af225d5f0eebe04775797ae
Fahrettin FILIZ <mobileuygulama@gmail.com>
2025-01-17 11:36:43 +0000

    feat: refactor PDF processing logic into a dedicated function; streamline database connection and error handling

--- getData_ff/src/createFinancialDatabaseTables.py
+++ getData_ff/src/createFinancialDatabaseTables.py
@@ -8,17 +8,7 @@ import os
 
 from database.database_manager import DatabaseManager
 
-# Database configuration
-db_params = {
-    "host": "timescaledb",
-    "database": "backtofuture",
-    "user": "backtofuture",
-    "password": "back2future"
-}
 
-
-# Main script
-db_manager = DatabaseManager(db_params,schema="temelanaliz")
 
 def camel_case(s):
     """
@@ -68,56 +58,76 @@ def is_continuing_table(table, page):
     return True
 
 
-pdf_directory = "/root/kap_pdfs/"
-
-for pdf_file in os.listdir(pdf_directory):
-    if pdf_file.endswith(".pdf"):
-        print(f"Processing {pdf_file}")
-        pdf_path = os.path.join(pdf_directory, pdf_file)
-        with pdfplumber.open(pdf_path) as pdf:
-            first_page = pdf.pages[0]
-            text = first_page.extract_text()
+def process_pdf(pdf_path, db_manager):
+    with pdfplumber.open(pdf_path) as pdf:
+        first_page = pdf.pages[0]
+        text = first_page.extract_text()
+        
+        # Extract company name
+        company_match = re.search(r"KAMUYU AYDINLATMA PLATFORMU\s+(.+?)\s+A\.Ş\.", text)
+        if company_match:
+            company_name = company_match.group(1)
+            company_name = company_name + " A.Ş."
             
-            # Extract company name
-            company_match = re.search(r"KAMUYU AYDINLATMA PLATFORMU\s+(.+?)\s+A\.Ş\.", text)
-            if company_match:
-                company_name = company_match.group(1)
-                company_name = company_name + " A.Ş."
-                
-                # Select the symbol from the data_bist_stocks table based on the company name
-                select_symbol_query = """
-                SELECT symbol FROM data_bist_stocks WHERE company_name = %s;
-                """
-                db_manager.cursor.execute(select_symbol_query, (company_name,))
-                result = db_manager.cursor.fetchone()
-                if result:
-                    symbol = result[0]
-                else:
-                    print(f"No symbol found for company name: {company_name}")
-                    symbol = re.sub(r'\s+', '', company_name)  # Remove spaces and new lines
-                    # continue  # Skip processing this PDF if no symbol is found
-            
-            # Extract specific text between "Finansal Rapor" and "Finansal Tablolara ilişkin Genel Açıklama"
-            report_match = re.search(r"Finansal Rapor\s+(.*?)\s+Finansal Tablolara ilişkin Genel Açıklama", text, re.DOTALL)
-            if report_match:
-                report_info = report_match.group(1).strip()
-                report_info_cleaned = re.sub(r'\s+', '', report_info)  # Remove spaces and new lines
-                print(f"Report Info: {report_info_cleaned}")
+            # Select the symbol from the data_bist_stocks table based on the company name
+            select_symbol_query = """
+            SELECT symbol FROM data_bist_stocks WHERE company_name = %s;
+            """
+            db_manager.cursor.execute(select_symbol_query, (company_name,))
+            result = db_manager.cursor.fetchone()
+            if result:
+                symbol = result[0]
             else:
-                print("No report info found.")
-                continue  # Skip processing this PDF if no report info is found
-            
-            accumulated_table = []
-            for page_num, page in enumerate(pdf.pages, start=1):
-                tables = page.extract_tables()
-                if tables:
-                    last_table = tables[-1]  # Take only the last table on the page
-                    if is_continuing_table(last_table, page):
-                        accumulated_table.extend(last_table)  # Skip headers
-                    else:
-                        if accumulated_table:
-                            save_temel_analiz_table_to_db(accumulated_table, page_num, db_manager, symbol, report_info_cleaned)
-                            accumulated_table = []
-                        accumulated_table.extend(last_table)
-            if accumulated_table:
-                save_temel_analiz_table_to_db(accumulated_table, page_num, db_manager, symbol, report_info_cleaned)
+                print(f"No symbol found for company name: {company_name}")
+                symbol = re.sub(r'\s+', '', company_name)  # Remove spaces and new lines
+                # continue  # Skip processing this PDF if no symbol is found
+        
+        # Extract specific text between "Finansal Rapor" and "Finansal Tablolara ilişkin Genel Açıklama"
+        report_match = re.search(r"Finansal Rapor\s+(.*?)\s+Finansal Tablolara ilişkin Genel Açıklama", text, re.DOTALL)
+        if report_match:
+            report_info = report_match.group(1).strip()
+            report_info_cleaned = re.sub(r'\s+', '', report_info)  # Remove spaces and new lines
+            print(f"Report Info: {report_info_cleaned}")
+        else:
+            print("No report info found.")
+            return False  # Skip processing this PDF if no report info is found
+        
+        accumulated_table = []
+        for page_num, page in enumerate(pdf.pages, start=1):
+            tables = page.extract_tables()
+            if tables:
+                last_table = tables[-1]  # Take only the last table on the page
+                if is_continuing_table(last_table, page):
+                    accumulated_table.extend(last_table)  # Skip headers
+                else:
+                    if accumulated_table:
+                        save_temel_analiz_table_to_db(accumulated_table, page_num, db_manager, symbol, report_info_cleaned)
+                        accumulated_table = []
+                    accumulated_table.extend(last_table)
+        if accumulated_table:
+            save_temel_analiz_table_to_db(accumulated_table, page_num, db_manager, symbol, report_info_cleaned)
+        return True
+
+
+def main():
+    pdf_directory = "/root/kap_pdfs/"
+    # Database configuration
+    db_params = {
+        "host": "timescaledb",
+        "database": "backtofuture",
+        "user": "backtofuture",
+        "password": "back2future"
+    }
+
+    # Main script
+    db_manager = DatabaseManager(db_params,schema="temelanaliz")
+
+    for pdf_file in os.listdir(pdf_directory):
+        if pdf_file.endswith(".pdf"):
+            print(f"Processing {pdf_file}")
+            pdf_path = os.path.join(pdf_directory, pdf_file)
+            if not process_pdf(pdf_path, db_manager):
+                continue
+
+if __name__ == "__main__":
+    main()
