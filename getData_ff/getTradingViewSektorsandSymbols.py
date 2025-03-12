0ede239abe968396dd1eee8c6f1b6c4f19920ef7
Fahrettin FILIZ <mobileuygulama@gmail.com>
2025-03-12 10:00:39 +0000

    feat: add new script to fetch and save sector and industry data from TradingView; implement error handling and retry logic

--- getData_ff/getTradingViewSektorsandSymbols.py
+++ getData_ff/getTradingViewSektorsandSymbols.py
@@ -3,6 +3,7 @@ import json
 import psycopg2
 from datetime import datetime, timedelta
 from requests.exceptions import RequestException, JSONDecodeError
+from bs4 import BeautifulSoup
 
 # Database connection parameters
 # engine now support this feature beaceuse it gets other data for run
@@ -18,26 +19,26 @@ conn = psycopg2.connect(**db_params)
 cursor = conn.cursor()
 
 # Create table if not exists
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS historical_price_emtia (
-        id SERIAL PRIMARY KEY,
-        date DATE NOT NULL,
-        kur_temInatkodu_D REAL,
-        kur_temInatkodu_EU REAL,
-        rfrns_AG REAL,
-        rfrns_AU REAL,
-        rfrns_PD REAL,
-        rfrns_PT REAL
-    )
-''')
+# cursor.execute('''
+#     CREATE TABLE IF NOT EXISTS historical_price_emtia (
+#         id SERIAL PRIMARY KEY,
+#         date DATE NOT NULL,
+#         kur_temInatkodu_D REAL,
+#         kur_temInatkodu_EU REAL,
+#         rfrns_AG REAL,
+#         rfrns_AU REAL,
+#         rfrns_PD REAL,
+#         rfrns_PT REAL
+#     )
+# ''')
 
 # Function to remove null values from the response content
 def remove_nulls_from_content(content):
     return content.replace(b'null', b'{}')
 
 # Function to fetch and save data for a specific date range and price type
-def fetch_and_save_data(start_date, end_date, price_type):
-    url = f"https://www.borsaistanbul.com/datfile/kmtprfrnstrh?startDate={start_date}&endDate={end_date}&priceType={price_type}"
+def fetch_and_save_data():
+    url = f"https://tr.tradingview.com/markets/stocks-turkey/sectorandindustry-sector/"
     print(f"url: {url}")
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
@@ -59,7 +60,10 @@ def fetch_and_save_data(start_date, end_date, price_type):
                         return
                     continue
                 cleaned_content = remove_nulls_from_content(response.content)
-                data = json.loads(cleaned_content)
+                soup = BeautifulSoup(cleaned_content, 'html.parser')
+                sectors = soup.select('tbody tr.listRow a.tickerLinkCell-ZOeSlGQR')
+                for sector in sectors:
+                    print(sector.text)
                 break
             else:
                 print(f"No content returned for URL: {url}")
@@ -72,54 +76,11 @@ def fetch_and_save_data(start_date, end_date, price_type):
             if retries == 0:
                 return
 
-    # Extract relevant data
-    rfrnsTrh = data.get("rfrnsTrh", [])
-    if rfrnsTrh is None:
-        rfrnsTrh = []
-
-    for item in rfrnsTrh:
-        prc_date = item.get("prc_date")
-        takasfiyat = item.get("takasfiyat")
-        takasfiyat = takasfiyat / 1000  # Convert kg to gram
-
-        # Check if the date already exists in the database
-        cursor.execute('SELECT id FROM historical_price_emtia WHERE date = %s', (prc_date,))
-        result = cursor.fetchone()
-
-        if result:
-            # Update the existing row
-            try:
-                cursor.execute(f'''
-                    UPDATE historical_price_emtia
-                    SET rfrns_{price_type} = %s
-                    WHERE date = %s
-                ''', (takasfiyat, prc_date))
-            except psycopg2.Error as e:
-                print(f"Error updating data for {prc_date}: {e}")
-                conn.rollback()
-            else:
-                conn.commit()
-        else:
-            # Insert a new row
-            try:
-                cursor.execute(f'''
-                    INSERT INTO historical_price_emtia (date, rfrns_{price_type})
-                    VALUES (%s, %s)
-                ''', (prc_date, takasfiyat))
-            except psycopg2.Error as e:
-                print(f"Error inserting data for {prc_date}: {e}")
-                conn.rollback()
-            else:
-                conn.commit()
-
-# Define the date range and price types
-end_date = datetime.now().strftime("%Y%m%d")
-start_date = (datetime.now() - timedelta(days=7)).strftime("%Y%m%d")
-price_types = ["AG", "AU", "PD", "PT"]
+  
 
 # Fetch and save data for the specified date range and price types
-for price_type in price_types:
-    fetch_and_save_data(start_date, end_date, price_type)
+
+fetch_and_save_data()
 
 # Close the database connection
 conn.close()
